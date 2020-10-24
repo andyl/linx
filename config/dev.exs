e5f90a472565c74579da37af7e764b6b2e856618
andyl <andy@r210.com>
2020-10-24 09:22:17 -0700

    Change ports (dev:4050, prod: 5050)

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :linx, Linx.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :linx, LinxWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4050],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
